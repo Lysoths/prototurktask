6a6b79c90af9aad7e8fb7d0e618c9dc8a59600a0
Oğuzhan ER <95685721+Lysoths@users.noreply.github.com>
2023-02-02 23:02:59 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [route, setRoute] = useState({
+    route: "",
+    station: "",
+  });
+
+  const handleChange = (e) => {
+    setRoute((route) => ({
+      ...route,
+      route: e.target.value,
+    }));
+    console.log(route);
+  };
+  const handleSubmit = (e) => {
+    setRoute((route) => ({
+      ...route,
+      station: [
+        ...route.station,
+        {
+          name: "",
+          lat: "",
+          lon: "",
+          errors: "",
+        },
+      ],
+    }));
+  };
+
+  const handleChanges = (value, name, key) => {
+    setRoute((route) => ({
+      ...route,
+      station: route.station.map((station, i) => {
+        if (key === i) {
+          station[name] = value;
+          let current = route.station.find(
+            (el, i) => el[name] === value && i !== key
+          );
+          if (current) {
+            station.errors = `${name} değeri daha önce ${value} değeriyle oluşturulmuş`;
+          }
+        }
+        return station;
+      }),
+    }));
+    console.log(route.station.map((item) => item));
+  };
+  const isEnable =
+    route.route &&
+    route.station &&
+    route.station.every((station) =>
+      Object.entries(station).every(([key, value]) =>
+        key === "errors" ? Object.values(value).length === 0 : value
+      )
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <input
+          type='text'
+          value={route.route}
+          placeholder='Güzergah'
+          onChange={handleChange}
+        />
+        <input
+          type='submit'
+          value='Yeni Durak Ekle'
+          onClick={handleSubmit}
+        ></input>
+      </div>
+      <hr />
+
+      {route.station &&
+        route.station.map((item, key) => (
+          <div key={key}>
+            <input
+              type='text'
+              placeholder='Durak Adı'
+              onChange={(e) => handleChanges(e.target.value, "name", key)}
+            />
+            <input
+              type='text'
+              placeholder='Enlem'
+              onChange={(e) => handleChanges(e.target.value, "lat", key)}
+            />
+            <input
+              type='text'
+              name=''
+              id=''
+              placeholder='Boylam'
+              onChange={(e) => handleChanges(e.target.value, "lon", key)}
+            />
+            <hr />
+          </div>
+        ))}
+
+      <div>
+        <input type='submit' value='Kaydet' disabled={!isEnable}></input>
+      </div>
     </div>
   );
 }
